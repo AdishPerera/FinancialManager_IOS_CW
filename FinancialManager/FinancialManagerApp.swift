b092546e7d5305bee5868b28c21710632636e497
AdishPerera <adishperera4@gmail.com>
2023-09-21 17:14:05 +0530

    Initial Commit

---
+++ FinancialManager/FinancialManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialManagerApp.swift
+//  FinancialManager
+//
+//  Created by Adish Perera on 2023-09-21.
+//
+
+import SwiftUI
+
+@main
+struct FinancialManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
